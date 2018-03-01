84900f5c47dcc8c819778eea1aa2551ba973b589
Mallory Bedwell <mebedwell@gmail.com>
2018-03-01 10:12:39 -0800

    create .py script

---
+++ analysis.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#saved as analysis.py
+
+import pandas as pd
+
+dat = pd.read_csv('data/gapminder_gdp_europe.csv')
